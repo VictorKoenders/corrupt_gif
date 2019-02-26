f48193a1b7514cac2af9525bd824ef76a66a7d6a
Victor Koenders <victor.koenders@gmail.com>
2019-02-26 08:10:52 +0000

    Parallelized the gif corruption

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
-extern crate gif;
-extern crate rand;
-extern crate image;
-
-use image::GenericImage;
-use std::io::Read;
-use std::fs::File;
-use std::path::Path;
+use image::GenericImageView;
+use pbr::ProgressBar;
 use rand::Rng;
-
+use rayon::prelude::*;
+use std::borrow::Cow;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
 
 fn main() {
     let arg = match std::env::args().nth(1) {
@@ -32,62 +30,76 @@ fn main() {
 
     let mut buff = Vec::new();
     file.read_to_end(&mut buff).expect("Could not read file");
+    let buff = buff;
 
     let mut out_file = File::create(&out_file_name).expect("Could not create output gif");
     let mut rand = rand::thread_rng();
-    println!("{:?}", &buff[..10]);
     let format = image::guess_format(&buff).expect("Could not guess image format");
     if format != image::ImageFormat::JPEG {
         println!("This program has only been tested to work with JPG/JPEG");
         println!("Use at own risk");
     }
-    let dimensions = image::load_from_memory_with_format(&buff, format).expect("Could not load image").dimensions();
+    let dimensions = image::load_from_memory_with_format(&buff, format)
+        .expect("Could not load image")
+        .dimensions();
     let dimensions = (dimensions.0 as u16, dimensions.1 as u16);
-    
-    let mut encoder = gif::Encoder::new(&mut out_file, dimensions.0, dimensions.1, &[]).expect("Could not create gif encoder");
-    add_frame(&buff, format, &mut encoder, &dimensions).expect("Could not write first frame");
 
-    for i in 0..100 {
-        let mut fail_count = 0;
-        loop {
-            // corrupt a random pixel
-            let index = rand.gen_range(0, buff.len());
-            let value = rand.gen_range(0, (std::u8::MAX as u16) + 1) as u8;
-            let old_value = buff[index];
-            buff[index] = value;
+    let mut encoder = gif::Encoder::new(&mut out_file, dimensions.0, dimensions.1, &[])
+        .expect("Could not create gif encoder");
 
-            if let Ok(()) = add_frame(&buff, format, &mut encoder, &dimensions) {
-                break;
-            }
-            buff[index] = old_value;
-            fail_count += 1;
-            if fail_count == 1_000_000 {
-                println!("Failed too many times, aborting");
-                std::fs::remove_file(&out_file_name).expect("Could not delete output gif");
-                return;
-            }
-        }
-        println!("{}%", i + 1);
+    println!("Starting the generation of frames");
+
+    let mut frames = Vec::with_capacity(120);
+
+    frames.push(buff.clone());
+
+    for _ in 0..119 {
+        let index = rand.gen_range(0, buff.len());
+        let value = rand.gen_range(0, u16::from(std::u8::MAX) + 1) as u8;
+        let mut buff = buff.clone();
+        buff[index] = value;
+        frames.push(buff);
     }
+
+    let frames = frames
+        .par_iter()
+        .filter_map(|buff| {
+            let image = match image::load_from_memory_with_format(&buff, format) {
+                Err(e) => {
+                    println!("Could not load image {:?}", e);
+                    return None;
+                }
+                Ok(image) => image,
+            };
+
+            let rgba_image = image.to_rgba();
+            let raw = rgba_image.into_raw();
+
+            let mut frame = gif::Frame::default();
+            frame.width = dimensions.0;
+            frame.height = dimensions.1;
+            frame.buffer = Cow::Owned(raw);
+            frame.delay = 10;
+
+            Some(frame)
+        })
+        .collect::<Vec<_>>();
+
+    println!("Combining the frames into a gif, this can take a second");
+    let mut pb = ProgressBar::new((frames.len() as u64).max(100));
+    pb.format("[=> ]");
+    for frame in frames.into_iter().take(100) {
+        encoder.write_frame(&frame).expect("Could not add frame");
+        pb.inc();
+    }
+    pb.finish_print("Done");
 }
 
-// Create image from the in-memory buffer
-fn add_frame(buff: &[u8], format: image::ImageFormat, encoder: &mut gif::Encoder<&mut File>, dimensions: &(u16, u16)) -> Result<(), ()> {
-    let image = image::load_from_memory_with_format(&buff, format).map_err(|e|{
-        println!("{:?}", e);
-        ()
-    })?;
-    let rgba_image = image.to_rgba();
-    let mut raw = rgba_image.into_raw();
-
-    // Add the image to the gif encoder
-    let mut frame = gif::Frame::from_rgba(dimensions.0, dimensions.1, &mut raw);
-    frame.delay = 10;
-    encoder.write_frame(&frame).expect("Could not add frame");
-
-    Ok(())
-}
-
-fn print_help(){
-    println!("Usage: {} <filename>", std::env::args().nth(0).unwrap_or_else(|| String::from("corrupted_gif")));
+fn print_help() {
+    println!(
+        "Usage: {} <filename>",
+        std::env::args()
+            .nth(0)
+            .unwrap_or_else(|| String::from("corrupted_gif"))
+    );
 }
