8d00830c421f6d4520d1ec66e39621616903c256
Victor Koenders <victor.koenders@gmail.com>
2019-02-26 09:00:10 +0000

    Performance optimalizations

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use rand::Rng;
 use rayon::prelude::*;
 use std::borrow::Cow;
 use std::fs::File;
-use std::io::Read;
+use std::io::{Read, Write};
 use std::path::Path;
 
 fn main() {
@@ -32,7 +32,6 @@ fn main() {
     file.read_to_end(&mut buff).expect("Could not read file");
     let buff = buff;
 
-    let mut out_file = File::create(&out_file_name).expect("Could not create output gif");
     let mut rand = rand::thread_rng();
     let format = image::guess_format(&buff).expect("Could not guess image format");
     if format != image::ImageFormat::JPEG {
@@ -44,55 +43,79 @@ fn main() {
         .dimensions();
     let dimensions = (dimensions.0 as u16, dimensions.1 as u16);
 
-    let mut encoder = gif::Encoder::new(&mut out_file, dimensions.0, dimensions.1, &[])
-        .expect("Could not create gif encoder");
+    // The gif ends up being about 8 times the size of the original image size
+    // And we generate up to 120 frames
+    // So we reserve a buffer of roughly that size so we don't have to resize later
+    let initial_capacity = buff.len() * 120 * 8;
+    let mut writer = Vec::with_capacity(initial_capacity);
 
-    println!("Starting the generation of frames");
+    {
+        let mut encoder = gif::Encoder::new(&mut writer, dimensions.0, dimensions.1, &[])
+            .expect("Could not create gif encoder");
 
-    let mut frames = Vec::with_capacity(120);
+        println!("Corrupting the buffers");
 
-    frames.push(buff.clone());
+        let mut frames = Vec::with_capacity(120);
 
-    for _ in 0..119 {
-        let index = rand.gen_range(0, buff.len());
-        let value = rand.gen_range(0, u16::from(std::u8::MAX) + 1) as u8;
-        let mut buff = buff.clone();
-        buff[index] = value;
-        frames.push(buff);
+        frames.push((0, buff.clone()));
+
+        for _ in 0..119 {
+            let index = rand.gen_range(0, buff.len());
+            let value = rand.gen_range(0, u16::from(std::u8::MAX) + 1) as u8;
+            let mut buff = buff.clone();
+            buff[index] = value;
+            frames.push((index, buff));
+        }
+        println!("Starting the generation of frames");
+
+        let frames = frames
+            .par_iter()
+            .filter_map(|(index, buff)| {
+                let image = match image::load_from_memory_with_format(&buff, format) {
+                    Err(e) => {
+                        println!(
+                            "Could not load image, tried changing byte at {}: {:?}",
+                            index, e
+                        );
+                        return None;
+                    }
+                    Ok(image) => image,
+                };
+
+                let rgba_image = image.to_rgba();
+                let raw = rgba_image.into_raw();
+
+                let mut frame = gif::Frame::default();
+                frame.width = dimensions.0;
+                frame.height = dimensions.1;
+                frame.buffer = Cow::Owned(raw);
+                frame.delay = 10;
+
+                Some(frame)
+            })
+            .collect::<Vec<_>>();
+
+        println!("Combining the frames into a gif, this can take a second");
+        let mut pb = ProgressBar::new((frames.len() as u64).min(100));
+        pb.format("[=> ]");
+        for frame in frames.into_iter().take(100) {
+            encoder.write_frame(&frame).expect("Could not add frame");
+            pb.inc();
+        }
+        pb.finish_print("Done");
     }
 
-    let frames = frames
-        .par_iter()
-        .filter_map(|buff| {
-            let image = match image::load_from_memory_with_format(&buff, format) {
-                Err(e) => {
-                    println!("Could not load image {:?}", e);
-                    return None;
-                }
-                Ok(image) => image,
-            };
-
-            let rgba_image = image.to_rgba();
-            let raw = rgba_image.into_raw();
-
-            let mut frame = gif::Frame::default();
-            frame.width = dimensions.0;
-            frame.height = dimensions.1;
-            frame.buffer = Cow::Owned(raw);
-            frame.delay = 10;
-
-            Some(frame)
-        })
-        .collect::<Vec<_>>();
-
-    println!("Combining the frames into a gif, this can take a second");
-    let mut pb = ProgressBar::new((frames.len() as u64).max(100));
-    pb.format("[=> ]");
-    for frame in frames.into_iter().take(100) {
-        encoder.write_frame(&frame).expect("Could not add frame");
-        pb.inc();
-    }
-    pb.finish_print("Done");
+    println!("Writing {} bytes to the output gif", writer.len());
+    println!(
+        "Len is {}% of the initial capacity ({} => {})",
+        writer.len() * 100 / initial_capacity,
+        initial_capacity,
+        writer.len()
+    );
+    File::create(&out_file_name)
+        .expect("Could not create output gif")
+        .write_all(&writer)
+        .unwrap();
 }
 
 fn print_help() {
